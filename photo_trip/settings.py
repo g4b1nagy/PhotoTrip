2742110de15c50594d06001dc8baeddb8db641c3
Gabi Nagy <gabrian.nagy@gmail.com>
2024-10-07 19:09:45 +0200

    Use PostgreSQL

--- photo_trip/settings.py
+++ photo_trip/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "photo_trip.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "photo_trip",
+        "USER": "photo_trip",
+        "PASSWORD": "photo_trip",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
