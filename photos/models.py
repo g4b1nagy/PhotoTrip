b837d557b03719452c345fef69355497791d3c24
Gabi Nagy <gabrian.nagy@gmail.com>
2024-10-07 22:09:44 +0200

    Clean up photos/models.py

--- photos/models.py
+++ photos/models.py
@@ -1,11 +1,14 @@
 from utils.models import BaseModel
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Photo(BaseModel):
     thumbnail = models.FileField()
     file_name = models.CharField(max_length=256)
     file_path = models.CharField(max_length=4096)
+    is_image = models.BooleanField(null=True)
+    is_video = models.BooleanField(null=True)
 
     # Fields derived from os.stat():
     file_size = models.PositiveBigIntegerField()
@@ -13,7 +16,7 @@ class Photo(BaseModel):
     file_mtime = models.DateTimeField()
     file_ctime = models.DateTimeField()
 
-    # Fields derived from metadata:
+    # Fields derived from utils.exif:
     file_type = models.ForeignKey("FileType", on_delete=models.PROTECT)
     mime_type = models.ForeignKey("MimeType", on_delete=models.PROTECT)
     image_width = models.PositiveIntegerField(null=True)
@@ -23,8 +26,8 @@ class Photo(BaseModel):
     gps_latitude = models.FloatField(null=True)
     gps_longitude = models.FloatField(null=True)
     gps_altitude = models.FloatField(null=True)
-    camera = models.ForeignKey("Camera", null=True, on_delete=models.SET_NULL)
-    lens = models.ForeignKey("Lens", null=True, on_delete=models.SET_NULL)
+    camera = models.ForeignKey("Camera", null=True, on_delete=models.PROTECT)
+    lens = models.ForeignKey("Lens", null=True, on_delete=models.PROTECT)
     metadata = models.JSONField()
 
     class Meta:
@@ -39,7 +42,7 @@ class Photo(BaseModel):
 
 
 class FileType(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=128)
 
     class Meta:
         constraints = [
@@ -51,7 +54,7 @@ class FileType(BaseModel):
 
 
 class MimeType(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=128)
 
     class Meta:
         constraints = [
@@ -63,42 +66,40 @@ class MimeType(BaseModel):
 
 
 class Camera(BaseModel):
-    make = models.CharField(max_length=64)
-    model = models.CharField(max_length=64)
-    serial_number = models.CharField(max_length=64)
+    make = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
 
     class Meta:
         constraints = [
             models.CheckConstraint(
-                check=~models.Q(make="")
-                | ~models.Q(model="")
-                | ~models.Q(serial_number=""),
+                check=~models.Q(make="") | ~models.Q(model=""),
                 name="non_empty_camera",
             ),
-            models.UniqueConstraint(
-                fields=["make", "model", "serial_number"], name="unique_camera"
-            ),
+            models.UniqueConstraint(fields=["make", "model"], name="unique_camera"),
         ]
 
     def __str__(self):
-        return f"{self.make} {self.model} {self.serial_number}"
+        return f"{self.make} {self.model}".strip()
 
 
 class Lens(BaseModel):
-    make = models.CharField(max_length=64)
-    name = models.CharField(max_length=256)
-    serial_number = models.CharField(max_length=64)
+    class Position(models.IntegerChoices):
+        BACK = 0, _("back camera")
+        FRONT = 1, _("front camera")
+
+    make = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+    position = models.IntegerField(choices=Position, null=True)
 
     class Meta:
         constraints = [
             models.CheckConstraint(
-                check=~models.Q(name=""), name="non_empty_lens_name"
+                check=~models.Q(make="") | ~models.Q(model=""),
+                name="non_empty_lens",
             ),
-            models.UniqueConstraint(
-                fields=["make", "name", "serial_number"], name="unique_lens"
-            ),
+            models.UniqueConstraint(fields=["make", "model"], name="unique_lens"),
         ]
         verbose_name_plural = "lenses"
 
     def __str__(self):
-        return f"{self.make} {self.name} {self.serial_number}"
+        return f"{self.make} {self.model}".strip()
